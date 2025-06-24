8f5a94d9915c17b133ec6d3f2feeec0f67bced11
DDM <22308829@qq.com>
2025-06-24 17:09:44 +0800

    解决无法生成daisyUI CSS的问题

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},  // 处理 @import
-    'tailwindcss': {
-      config: './tailwind.config.js' // 指定 Tailwind CSS 配置文件
-    },
-    'autoprefixer': {}     // 自动添加浏览器前缀
-  }
-}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/renderer/**/*.{html,js}"],// 扫描渲染进程的 HTML/JS 文件
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")]
-}
